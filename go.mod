b08c3dd5ed355fc85790ed1239af3cc47b3c2e76
Adrian Witas <adrianwit@gmail.com>
2021-05-12 21:51:02 -0700

    patched generator

--- go.mod
+++ go.mod
@@ -10,10 +10,9 @@ require (
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/viant/afs v1.8.0
+	github.com/viant/afs v1.12.0
 	github.com/viant/toolbox v0.34.2
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	github.com/viant/parquet v0.0.0-20210511002257-ab3b508acedf
 )
--- parquet/codegen/template.go
+++ parquet/codegen/template.go
@@ -7,16 +7,16 @@ import (
 	"text/template"
 )
 
-//var tmap = template.FuncMap{
-//	"inc": func(i int) int {
-//		return i + 1
-//	},
-//}
+var tmap = template.FuncMap{
+	"dec": func(i int) int {
+		return i - 1
+	},
+}
 
 type Template string
 
 func (t Template) Expand(id string, data interface{}) (string, error) {
-	aTemplate, err := template.New(id).Parse(string(t))
+	aTemplate, err := template.New(id).Funcs(tmap).Parse(string(t))
 	if err != nil {
 		panic(fmt.Sprintf("failed to parse template %v, due to %v", id, err))
 	}
